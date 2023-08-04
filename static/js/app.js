0587566d3db89fc9a78eff0d095d657335d67bcf
mavey14 <harveylencio@gmail.com>
2023-08-04 20:37:37 +0800

    added sudoku board

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,18 @@
 
-console.log('TESTSETSTST')
+const table = document.querySelector('.table')
+
+for(let i=0;i<9;i++){
+    var tr = document.createElement('tr')
+    tr.classList.add(`row-${i}`)
+    table.appendChild(tr)
+    for(let j=0;j<9;j++){
+        var td = document.createElement('td')
+        var newInput = document.createElement('input')
+        newInput.type='text'
+        newInput.maxLength=1
+        td.classList.add(`col-${j}`)
+        tr.appendChild(td)
+        td.appendChild(newInput)
+
+    }
+}
